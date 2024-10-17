d22c1f51ef779a6a740c332259dcba2c84e5fb12
Sergei Borodin <borodin.devel@gmail.com>
2024-10-18 02:01:19 +0300

    Integrate raylib

--- EcsTemplate.cpp
+++ EcsTemplate.cpp
@@ -3,6 +3,7 @@
 
 #include "EcsTemplate.h"
 
+#include "raylib-cpp.hpp"
 #include "spdlog/spdlog.h"
 
 #include "buildinfo.h"
@@ -20,6 +21,23 @@ int main()
               << "Path: " << g_currentWorkingDir.string() << std::endl;
 
     spdlog::info("Welcome to spdlog!");
+
+    int screenWidth = 800;
+    int screenHeight = 450;
+
+    raylib::Window window(screenWidth, screenHeight, "raylib-cpp - basic window");
+
+    SetTargetFPS(60);
+
+    while (!window.ShouldClose()) {
+        BeginDrawing();
+
+        window.ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
 
     return 0;
 }
